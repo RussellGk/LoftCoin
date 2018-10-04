2c555a4f9313e681145c43c06d1845f1d1b38d01
Russell <mediandroid@mail.com>
2018-10-04 15:00:04 +0300

    Add images in webp and Butterknife lib, WelcomeFragment with layout

---
+++ app/src/main/java/com/hardtm/loftcoin/screens/welcome/WelcomeFragment.java
@@ -0,0 +1,31 @@
+package com.hardtm.loftcoin.screens.welcome;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hardtm.loftcoin.R;
+
+public class WelcomeFragment extends Fragment {
+
+
+    public WelcomeFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_welcome, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
