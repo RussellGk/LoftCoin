bd03910aa6ccee1ebc4bd7cee813a175b1389ec6
Russell <mediandroid@mail.com>
2018-10-16 21:27:43 +0300

    Fix the adapter position in RecyclerView after an Activity was recreated

--- app/src/main/java/com/hardtm/loftcoin/screens/main/rate/RateFragment.java
+++ app/src/main/java/com/hardtm/loftcoin/screens/main/rate/RateFragment.java
@@ -3,6 +3,7 @@ package com.hardtm.loftcoin.screens.main.rate;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -30,6 +31,8 @@ import butterknife.Unbinder;
 
 public class RateFragment extends Fragment implements RateView {
 
+    private static final String LAYOUT_MANAGER_STATE = "layout_manager_state";
+
     @BindView(R.id.rate_recycler)
     RecyclerView recycler;
 
@@ -45,6 +48,7 @@ public class RateFragment extends Fragment implements RateView {
     private RatePresenter presenter;
     private RateAdapter adapter;
     private Unbinder unbinder;
+    private Parcelable layoutManagerState;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -83,6 +87,9 @@ public class RateFragment extends Fragment implements RateView {
                 presenter.onRefresh();
             }
         });
+        if(savedInstanceState != null){
+            layoutManagerState = savedInstanceState.getParcelable(LAYOUT_MANAGER_STATE);
+        }
         presenter.attechView(this);
         presenter.getRate();
     }
@@ -94,10 +101,19 @@ public class RateFragment extends Fragment implements RateView {
         super.onDestroyView();
     }
 
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putParcelable(LAYOUT_MANAGER_STATE, recycler.getLayoutManager().onSaveInstanceState());
+        super.onSaveInstanceState(outState);
+    }
 
     @Override
     public void setCoins(List<CoinEntity> coins) {
         adapter.setCoins(coins);
+        if(layoutManagerState != null) {
+            recycler.getLayoutManager().onRestoreInstanceState(layoutManagerState);
+            layoutManagerState = null;
+        }
 
     }
 
